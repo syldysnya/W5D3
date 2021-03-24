80da9601ce68b87080ea72c9586b63535f3b08e4
Andrew Kihs <ajkihs@gmail.com>
2021-03-24 15:44:33 -0400

    Move drop_tables to beginning of file

--- sql_project/import_db.sql
+++ sql_project/import_db.sql
@@ -1,6 +1,26 @@
 PRAGMA foreign_keys = ON;
 
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS question_likes;
 DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  fname TEXT NOT NULL,
+  lname TEXT NOT NULL
+);
+
+INSERT INTO
+  users (fname, lname)
+VALUES
+  ('Arthur', 'Miller'),
+  ('blah blah', 'Miller'),
+  ('testing', 'Miller'),
+  ('Eugene O','Neill');
+
+
 CREATE TABLE questions (
   id INTEGER PRIMARY KEY,
   title TEXT NOT NULL,
@@ -10,14 +30,14 @@ CREATE TABLE questions (
   FOREIGN KEY (associated_author) REFERENCES users(id)
 );
 
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-  id INTEGER PRIMARY KEY,
-  fname TEXT NOT NULL,
-  lname TEXT NOT NULL,
-);
+INSERT INTO
+    questions(title, body, associated_author)
+VALUES
+  ('All My Sons', 'body body body ', (SELECT id FROM users WHERE fname = 'Arthur')),
+  ('How do you use SQL?', 'body jalsdkfljalsdf', (SELECT id FROM users WHERE fname = 'testing'));
 
-DROP TABLE IF EXISTS question_follows;
+
+
 CREATE TABLE question_follows (
     id INTEGER PRIMARY KEY,
     follower_id INTEGER NOT NULL,
@@ -27,7 +47,7 @@ CREATE TABLE question_follows (
     FOREIGN KEY (following_id) REFERENCES users(id)
 );
 
-DROP TABLE IF EXISTS replies;
+
 CREATE TABLE replies (
     id INTEGER PRIMARY KEY,
     subject_question TEXT NOT NULL,
@@ -39,3 +59,22 @@ CREATE TABLE replies (
     FOREIGN KEY (qustion_parent) REFERENCES replies(id),
     FOREIGN KEY (reply_user) REFERENCES users(id)
 );
+
+
+
+CREATE TABLE question_likes(
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+
+
+INSERT INTO
+    question_follows(follower_id, following_id)
+VALUES
+    ((SELECT id FROM users WHERE fName = 'Arthur'),(SELECT id FROM users WHERE fName = 'testing')),
+    ((SELECT id FROM users WHERE fName = 'testing'),(SELECT id FROM users WHERE fName = 'blah blah'));              
