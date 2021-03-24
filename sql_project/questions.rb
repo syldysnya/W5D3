0ad8c9a72802e465d45f740dc737c021f04d1c9d
Syldys <syldysnya@gmail.com>
2021-03-24 17:28:06 -0400

    Add changes to ruby file

--- sql_project/questions.rb
+++ sql_project/questions.rb
@@ -13,6 +13,8 @@ end
 
 class Question
 
+    attr_accessor :id, :title, :body, :associated_author
+
     def self.find_by_id(id)
         # raise "#{id} not found in DB" unless self.id
         question = QuestionsDBConnection.instance.execute(<<-SQL, id)
@@ -32,4 +34,53 @@ class Question
         @body = options['body']
         @associated_other = options['associated_author']
     end
+
+end
+
+class Reply
+    
+    def self.find_by_user_id(author_id)
+        reply = QuestionsDBConnection.instance.execute(<<-SQL, author_id)
+        SELECT
+            *
+        FROM
+            replies
+        WHERE
+            author_id = ?
+        SQL
+    end
+    
+    def self.find_by_question_id(question_id)
+        question = QuestionsDBConnection.instance.execute(<<-SQL, question_id)
+        SELECT
+            *
+        FROM
+            replies
+        WHERE
+            question_id = ?
+        SQL
+    end
+
+    attr_accessor :id, :question_id, :author_id, :body, :parent_id
+
+    def initialize(options)
+        @id = options['id']
+        @question_id = options['question_id']
+        @author_id = options['author_id']
+        @body = options['body']
+        @parent_id = options['parent_id']
+    end
+end
+
+class User
+
+    attr_accessor :id, :fname, :lname
+
+    def initialize(options)
+         @id = options['id']
+         @fname = options['fname']
+         @lname = options['lname']
+    end
+
+    # def self.find_by_name(fname, lname)
 end
